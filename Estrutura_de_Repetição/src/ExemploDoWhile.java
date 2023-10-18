9496ce777a112b0493a83cd7c929bf94072a11ad
Venancius <steniovenancius@gmail.com>
2023-10-18 16:29:08 -0300

    Quarta

--- Estrutura_de_Repetição/src/ExemploDoWhile.java
+++ Estrutura_de_Repetição/src/ExemploDoWhile.java
@@ -1,7 +1,7 @@
 public class ExemploDoWhile {
     public static void main(String[] args) {
        int numero = 1;
-
+       
        for(int x=1; x<2; x++){
         numero = numero + x;
        }
